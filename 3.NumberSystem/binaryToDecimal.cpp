bb6fb49008778cbb762fe733b77391c3eb0a8eab
vishalthakur <vishalthakur970820@gmail.com>
2025-11-17 22:57:25 +0530

    Revision till 2d vector

--- 3.NumberSystem/binaryToDecimal.cpp
+++ 3.NumberSystem/binaryToDecimal.cpp
@@ -15,23 +15,24 @@ int binaryToDecimalMethod1(int n){
     return decimal;
 }
 
-// pending
-// int binaryToDecimalMethod2(int n){
-//     // Bitwise method
-//     int decimal=0;
-//     int i=0;
-//     while(n>0){
-//         int bit = n & 1;
-//         decimal = decimal + bit * pow(2,i++);
-//         n = n >> 1;
-//     }
-//     return decimal;
-// }
+
+int binaryToDecimalMethod2(int n){
+    // Bitwise method
+    int decimal=0;
+    int i=1;
+    while(n>0){
+        int bit = n & 1;
+        decimal = decimal + bit * i;
+        i = i << 1;
+        n = n >> 1;
+    }
+    return decimal;
+}
 
 int main(){
 
     int binary;
     cin >> binary;
 
-    cout << binaryToDecimalMethod1(binary) << endl;
+    cout << binaryToDecimalMethod2(binary) << endl;
 }
